3fa53e6607f142e9def8ea7da06888852c645a3f
Mike Mattice <mmattice@reliant.io>
2021-11-26 16:55:08 -0600

    Switch to a different openvpn mgmt module to stay connected

--- go.mod
+++ go.mod
@@ -2,12 +2,8 @@ module ovpn-health-check
 
 go 1.17
 
-require (
-	github.com/gorilla/mux v1.8.0
-	github.com/mmattice/go-openvpn v0.0.2
-)
 
 require (
-	github.com/sirupsen/logrus v1.8.1 // indirect
-	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/mmattice/go-openvpn-mgmt v0.0.0-20211126224253-86f82af9eebd
 )
--- main.go
+++ main.go
@@ -4,9 +4,10 @@ import (
 	"flag"
 	"fmt"
 	"github.com/gorilla/mux"
-	"github.com/mmattice/go-openvpn/server/mi"
+	"github.com/mmattice/go-openvpn-mgmt/openvpn"
 	"log"
 	"net/http"
+	"time"
 )
 
 var listenPort int
@@ -17,9 +18,9 @@ func init() {
 	const (
 		defaultHost       = "localhost"
 		usageHost         = "Management Interface IP"
-		defaultListenPort = 1196
+		defaultListenPort = 1194
 		usageListenPort   = "health check listen port"
-		defaultPort       = 11960
+		defaultPort       = 11940
 		usagePort         = "Management Interface Port"
 	)
 	flag.StringVar(&miHost, "host", defaultHost, usageHost)
@@ -30,22 +31,55 @@ func init() {
 	flag.IntVar(&miPort, "p", defaultPort, usagePort+" (shorthand)")
 }
 
+func connect(addr string, lsCh chan<- openvpn.LoadStat) {
+	for {
+		eventCh := make(chan openvpn.Event, 10)
+		var mgmt, err = openvpn.Dial(addr, eventCh)
+		if err != nil {
+			panic(err)
+		}
+		go func() {
+			for {
+				time.Sleep(1 * time.Second)
+				loadStat, err := mgmt.LoadStats()
+				if err != nil {
+					lsCh <- openvpn.LoadStat{Clients: -1, BytesIn: -1, BytesOut: -1}
+					break
+				} else {
+					lsCh <- loadStat
+				}
+			}
+		}()
+		for range eventCh {
+		}
+	}
+
+}
+
+var ls = openvpn.LoadStat{Clients: -1, BytesIn: -1, BytesOut: -1}
+
 func main() {
 	flag.Parse()
 	var r = mux.NewRouter()
+	lsCh := make(chan openvpn.LoadStat, 10)
+    go connect(fmt.Sprintf("%s:%d", miHost, miPort), lsCh)
+	go func() {
+		for loadStat := range lsCh {
+			ls = loadStat
+		}
+	}()
 	r.HandleFunc("/status", statusHandler)
-
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", listenPort), r))
 }
 
+
+
 func statusHandler(w http.ResponseWriter, _ *http.Request) {
-	var client = mi.NewClient("tcp", fmt.Sprintf("%s:%d", miHost, miPort))
-	var ls, err = client.GetLoadStats()
-	if err != nil {
+	if ls.Clients < 0 {
 		w.WriteHeader(http.StatusServiceUnavailable)
-		_, err = w.Write([]byte("OpenVPN Server Unavailable - Cannot connect\n"))
+		_, _ = w.Write([]byte("OpenVPN Server Unavailable - Cannot connect\n"))
 	} else {
 		w.WriteHeader(http.StatusOK)
-		_, err = w.Write([]byte(fmt.Sprintf("OpenVPN Healthy %d %d %d\n", ls.NClients, ls.BytesIn, ls.BytesOut)))
+		_, _ = w.Write([]byte(fmt.Sprintf("OpenVPN Healthy %d %d %d\n", ls.Clients, ls.BytesIn, ls.BytesOut)))
 	}
 }
